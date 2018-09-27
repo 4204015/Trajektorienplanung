3dde7b11112b4661e713f5e2e6042a98b08cbddf
4204015 <4204015@gmail.com>
2018-09-27 14:58:25 +0200

    Changes for presentation

--- plot.py
+++ plot.py
@@ -34,10 +34,10 @@ class PlotterLLM:
     # --- plot options 2D ---
     
     def result(self, ax):
-        ax.set_title("Lernergebnis")
-        ax.grid(True)
+        #ax.set_title("Lernergebnis")
+        #ax.grid(True)
         ax.plot(self.u, self.y, color=self.standard_colors[0], label="$y_{true}$")
-        ax.plot(self.u, self.y_pred, '--', color=self.standard_colors[2], label="$y_{approx}$")
+        ax.plot(self.u, self.y_pred, color=self.standard_colors[2], label="$y_{approx}$")
         
         #half_err = (self.y_pred - self.y)
         #upper = (self.y_pred + half_err).ravel()
@@ -55,7 +55,8 @@ class PlotterLLM:
         ax.get_yaxis().get_major_formatter().set_powerlimits((0, 0))
     
     def validity_function(self, ax):
-        ax.set_title("Zugehörigkeitsfunktionen")
+        #ax.set_title("Zugehoerigkeitsfunktionen")
+        ax.set_ylabel("$\Phi_i$")
         A = np.zeros((self.llm.M, self.u.shape[0]))
         self.llm.network.validity_function(self.u, A)
         for idx, column in enumerate(A[self.sort_idx, :]):
@@ -67,43 +68,44 @@ class PlotterLLM:
     def models(self, ax):
         #ax.set_title("Lokale Modelle")
         #ax.grid(True)
-        ax.plot(self.X_train, self.y_train, 'x', color=self.standard_colors[1],
-                markersize=5, alpha=0.5)#, label="$y_{train}$")
-        """
+        ax.set_ylabel("$y_i$")
+        #ax.plot(self.X_train, self.y_train, 'x', color=self.standard_colors[1],
+        #        markersize=5, alpha=0.5)#, label="$y_{train}$")
+        
         ax2 = ax.twinx()
         for idx, (u, y, c) in enumerate(local_model_gen(self.llm.network.model_range, self.llm.local_models.Theta_,
                                                         self.llm.network.C, self.llm.network.N_, self.sort_idx,
                                                         self.llm.local_models.poly)):
             
             # bar plot with samples per model
-            
+            """
             number_of_samples = np.sum(self.llm.network.A[self.sort_idx, :][idx, :])
             ax2.bar(self.llm.C[:, self.sort_idx][:, idx], number_of_samples, width=5*number_of_samples/self.X_train.shape[0], alpha=0.25, color=self.colors[idx])
-            
+            """
 
             ax.plot(u, y, color=self.colors[idx])
-            markerline, stemlines, _ = ax.stem(self.llm.C[:, self.sort_idx][:, idx], c, '--')
-            plt.setp(markerline, color=self.colors[idx], alpha=0.5)
-            plt.setp(stemlines, color=self.colors[idx], alpha=0.5)
+            #markerline, stemlines, _ = ax.stem(self.llm.C[:, self.sort_idx][:, idx], c, '--')
+            #plt.setp(markerline, color=self.colors[idx], alpha=0.5)
+            #plt.setp(stemlines, color=self.colors[idx], alpha=0.5)
 
             if self.llm.M <= 20:
                 ax.annotate(f"{self.sort_idx[idx]}", xy=(self.llm.C[:, self.sort_idx][:, idx], c),
                             xytext=(-4, 5), textcoords='offset points')
             
-        ax.legend()
-        """
+        #ax.legend()
+        
     def report(self, ax):
         
         # linear regression
         grad = 2 * self.llm.training_duration * 1000 / (self.llm.M + 1)**2
                 
         m, s = divmod(self.llm.training_duration, 60)
-        ax.set_title(f"Bericht\nTrainingszeit: {int(m)}:{s:.2f} | "
-                     + "$rmse_{min}$=" + f"{np.min(self.llm.global_loss):.4e}"
-                     + r" | $\bar{t}_{grad} =$" + f"{grad:.4f}")
+        #ax.set_title(f"Bericht\nTrainingszeit: {int(m)}:{s:.2f} | "
+        #             + "$rmse_{min}$=" + f"{np.min(self.llm.global_loss):.4e}"
+        #             + r" | $\bar{t}_{grad} =$" + f"{grad:.4f}")
         ax.grid(True)
-        ax.plot(range(1, self.llm.M + 1), self.llm.global_loss, color=self.standard_colors[0], label="global loss")
-        ax.set_ylabel("Global Loss")
+        ax.plot(range(1, self.llm.M + 1), self.llm.global_loss, color=self.standard_colors[0], label="RMSE")
+        ax.set_ylabel("RMSE")
         ax.set_xlabel("Anzahl Teilmodelle")
         ax.semilogy(True)
         ax.set_xlim(left=1)
@@ -111,11 +113,11 @@ class PlotterLLM:
                                 lw=1, color='red', axes=ax, linestyle='dashed')
         ax.add_line(tol_line)
     
-        ax_ = ax.twinx()
-        d = np.array(self.llm.network.split_duration)*1000
-        ax_.plot(range(1, self.llm.M), d, color=self.standard_colors[1], alpha=0.5)
-        ax_.set_ylabel('Dauer pro Split in $ms$', color=self.standard_colors[1])
-        ax_.tick_params('y', colors=self.standard_colors[1])
+        #ax_ = ax.twinx()
+        #d = np.array(self.llm.network.split_duration)*1000
+        #ax_.plot(range(1, self.llm.M), d, color=self.standard_colors[1], alpha=0.5)
+        #ax_.set_ylabel('Dauer pro Split in $ms$', color=self.standard_colors[1])
+        #ax_.tick_params('y', colors=self.standard_colors[1])
 
     def volume(self, ax):
         volume = []
@@ -239,8 +241,8 @@ class PlotterLLM:
             self.options = ["report"]
 
         # define colors for each model
-        self.standard_colors = plt.get_cmap('plasma')(np.linspace(0, 0.9, 4))
-        self.colors = plt.get_cmap('inferno')(np.linspace(0, 0.9, llm.M))
+        self.standard_colors = plt.get_cmap('plasma')(np.linspace(0, 0.7, 4))
+        self.colors = plt.get_cmap('inferno')(np.linspace(0, 0.7, llm.M))
         
         if self.sort and u.shape[1] <= 1:
             self.sort_idx = np.argsort(llm.C, axis=1)[0]
